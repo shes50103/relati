90b941de9b2dae0723e6de65592dc6bc270ffda8
shes50103 <shes50103@gmail.com>
2020-10-31 15:50:00 +0800

    ORM foreign_key class_name source

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,6 @@
 class Team < ApplicationRecord
   has_many :user_teams
-  has_many :users, through: :user_teams
+  # has_many :users, through: :user_teams
+
+  has_many :users, through: :user_teams, source: :my_user
 end
--- app/models/user_team.rb
+++ app/models/user_team.rb
@@ -1,4 +1,6 @@
 class UserTeam < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
   belongs_to :team
+
+  belongs_to :my_user, foreign_key: :user_id, class_name: "User"
 end
